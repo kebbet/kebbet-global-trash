8932f3e525a21656d5255952beaa59be44116401
Erik <11491369+kebbet@users.noreply.github.com>
2023-08-31 12:50:01 +0200

    Adds license data, minor cs fixes

--- kebbet-global-trash.php
+++ kebbet-global-trash.php
@@ -2,15 +2,18 @@
 /**
  * Plugin Name:       Kebbet plugins - Global trash for a site
  * Plugin URI:        https://github.com/kebbet/kebbet-global-trash
- * Description:       Adds a global trash to handle all items in trash.
- * Author:            Erik Betshammar
+ * Description:       Adds a tools page with a global trash list, to handle all trashed post items.
  * Version:           1.0.1
  * Network:           true
+ * Author:            Erik Betshammar
  * Author URI:        https://verkan.se
- * Requires at least: 6.3
+ * Requires at least: 6.2
+ * Tested up to:      6.3
  * Requires PHP:      7.4
  * Update URI:        false
  * Text domain:       kebbet-global-trash
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  *
  * @author Erik Betshammar
  * @package kebbet-global-trash
@@ -25,7 +28,7 @@ const KEBBET_GLOBAL_TRASH_VERSION = '1.0.1';
 function init() {
 	load_textdomain();
 }
-add_action( 'init', __NAMESPACE__ . '\init', 0 );
+add_action( 'init', __NAMESPACE__ . '\init' );
 
 /**
  * Load plugin textdomain.
